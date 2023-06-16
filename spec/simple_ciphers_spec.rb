89c1bb2ce1cbb52a35cfbc3ea90bd315a789198f
Kuba Jasko <kubajasko@hotmail.co.uk>
2023-06-16 12:23:50 +0100

    INITIAL COMMIT

---
+++ spec/simple_ciphers_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SimpleCiphers do
+  it "has a version number" do
+    expect(SimpleCiphers::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
